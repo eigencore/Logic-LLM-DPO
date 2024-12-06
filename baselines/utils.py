16ca579eff46843c20e1d6f5de27566dd7d959dd
MaxGalindo150 <maximilianogalindo7@gmail.com>
2024-12-06 14:30:42 -0600

    creating gpt model

--- baselines/utils.py
+++ baselines/utils.py
@@ -1,11 +1,10 @@
 import backoff  # for exponential backoff; allows for retries
 import openai
-import os
 import asyncio
 from typing import Any
 
 @backoff.on_exception(backoff.expo, openai.error.RateLimitError)
-def complitions_with_backoff(**kwargs):
+def completions_with_backoff(**kwargs):
     """
     This function is a wrapper around the OpenAI.Completion.create function.
     
@@ -108,6 +107,7 @@ class OpenAIModel:
         self.max_new_tokens = max_new_tokens
         self.stop_words = stop_words
         
+    # Used by openai ChatGPT    
     def chat_generate(self, input_string, temperature=0.0):
         response = chat_completions_with_backoff(
             model = self.model_name,
@@ -121,5 +121,72 @@ class OpenAIModel:
         )
         generated_text = response['choices'][0]['message']['content'].strip()
         return generated_text
+    
+    # used for text/code-davinci
+    def prompt_generate(self, input_string, temperature = 0.0):
+        response = completions_with_backoff(
+            model = self.model_name,
+            prompt = input_string,
+            max_tokens = self.max_new_tokens,
+            temperature = temperature,
+            top_p = 1.0,
+            frequency_penalty = 0.0,
+            presence_penalty = 0.0,
+            stop = self.stop_words
+        )
+        generated_text = response['choices'][0]['text'].strip()
+        return generated_text
+    
+    def generate(self, input_string, temperature = 0.0):
+        if self.model_name in ['text-davinci-002', 'code-davinci-002', 'text-davinci-003']:
+            return self.prompt_generate(input_string, temperature)
+        elif self.model_name in ['gpt-4', 'gpt-3.5-turbo']:
+            return self.chat_generate(input_string, temperature)
+        else:
+            raise Exception("Model name not recognized")
+        
+    def batch_chat_generate(self, messages_list, temperature = 0.0):
+        open_ai_messages_list = []
+        for message in messages_list:
+            open_ai_messages_list.append(
+                [{"role": "user", "content": message}]
+            )
+        predictions = asyncio.run(
+            dispatch_openai_chat_requests(
+                    open_ai_messages_list, self.model_name, temperature, self.max_new_tokens, 1.0, self.stop_words
+            )
+        )
+        return [x['choices'][0]['message']['content'].strip() for x in predictions]
+    
+    def batch_prompt_generate(self, prompt_list, temperature = 0.0):
+        predictions = asyncio.run(
+            dispatch_openai_prompt_requests(
+                    prompt_list, self.model_name, temperature, self.max_new_tokens, 1.0, self.stop_words
+            )
+        )
+        return [x['choices'][0]['text'].strip() for x in predictions]
+    
+    def batch_generate(self, messages_list, temperature = 0.0):
+        if self.model_name in ['text-davinci-002', 'code-davinci-002', 'text-davinci-003']:
+            return self.batch_prompt_generate(messages_list, temperature)
+        elif self.model_name in ['gpt-4', 'gpt-3.5-turbo']:
+            return self.batch_chat_generate(messages_list, temperature)
+        else:
+            raise Exception("Model name not recognized")
+        
+    def generate_insertion(self, input_string, suffix, temperature = 0.0):
+        response = completions_with_backoff(
+            model = self.model_name,
+            prompt = input_string,
+            suffix= suffix,
+            temperature = temperature,
+            max_tokens = self.max_new_tokens,
+            top_p = 1.0,
+            frequency_penalty = 0.0,
+            presence_penalty = 0.0
+        )
+        generated_text = response['choices'][0]['text'].strip()
+        return generated_text
+
     
     
